976d743cdbfaaf2bc0982c82dc537988ec607808
Dhanashree <dhanashreesarade919@gmail.com>
2021-06-05 13:36:44 +0530

    Add

---
+++ javascript.js
@@ -0,0 +1,93 @@
+let gameActive ="true";
+let currentPlayer ="X";
+let gameState=["","","","","","","","",""]
+let statusDisplay = document.querySelector(".game--status");
+
+function handleRestartGame(){
+gameActive=true;
+currentPlayer ="X";
+let gameState=["","","","","","","","",""];
+document.querySelectorAll(".cell").forEach(cell=> cell.innerHTML ="");
+statusDisplay.innerHTML=`Its ${currentPlayer}'s turn`;
+}
+
+function handleCellClick(clickedEvent){
+  const clickedCell = clickedEvent.target;
+ const clickedCellIndex = parseInt(clickedCell.getAttribute("data-cell-index"))
+if(gameState[clickedCellIndex] != "" || !gameActive){
+    return;
+}
+//   console.log(clickedCell)
+handleCellPlayer(clickedCell,clickedCellIndex);
+handleResultValidation();
+
+}
+
+function handleCellPlayer(clickedCell,clickedCellIndex){
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML=currentPlayer;
+}
+
+const winningConditions=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+
+
+
+
+function handleResultValidation(){
+    let roundWon = false;
+    for (let i=0 ; i<=7 ; i++){
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b= gameState[winCondition[1]];
+        let c= gameState[winCondition[2]]; 
+
+        if(a === "" || b === "" || c === ""){
+            continue;
+        }
+      
+        if( a===b && b===c){
+            roundWon= true;
+            break;
+        }
+    }
+
+        if(roundWon){
+            statusDisplay.innerHTML = `Congratulations,Player ${currentPlayer} has won.`;
+            gameActive = false;
+            return;
+        }
+
+       let roundDraw = !gameState.includes("");
+       if (roundDraw){
+           statusDisplay.innerHTML= `It's Draw`
+           gameActive = false;
+           return;
+       }
+        handlePlayer()
+    
+}
+
+function handlePlayer(){
+    // if(currentPlayer=== "X"){
+    //     currentPlayer === "O";
+    // }else{
+    //     currentPlayer ==="X";
+    // }
+
+    currentPlayer = currentPlayer === "X"? "O" : "X";
+    statusDisplay.innerHTML = `Its ${currentPlayer}'s turn`;
+}
+
+
+document.querySelector(".game--restart").addEventListener("click" , handleRestartGame);
+document.querySelectorAll(".cell").forEach(cell => cell.addEventListener("click" , handleCellClick))
